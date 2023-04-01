c438f5d68a17e9bcd8178b35c23c27e1f4d78ad1
Ksenia <99388976+MKJenna@users.noreply.github.com>
2023-04-01 11:39:32 +0300

    Add file via upload

---
+++ detect_elements.py
@@ -0,0 +1,29 @@
+import torch
+
+def detect_elements(image_path, p=0.5):
+    '''
+    Recognizes elements in an electrical circuit
+        
+    Parameters
+    ----------
+    
+    image_path: str
+        Location of the image for recognition (example: '/content/C116_D2_P1.jpg')
+        
+    p: int, default=0.5
+        Threshold of model confidence in the prediction, 0 <= p <= 1.
+        If an item is recognized with a probability lower
+        than the specified one, it will not be included.
+        
+    
+    Returns
+    ----------
+    df_results : pd.DataFrame
+    '''
+    # load model with pre-traind weights
+    model = torch.hub.load('ultralytics/yolov5', 'custom', 'yolo_weights.pt') 
+    # get classes and coordinations
+    df_results = model(image_path)
+    df_results = df_results.pandas().xyxy[0] 
+    df_results = df_results[annotation['confidence'] >= p]
+    return df_results
